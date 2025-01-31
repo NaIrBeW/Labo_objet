63abc441368b7dfd11e3d558229f1f0c1ec0d84f
Elodie Vachon <elodievachon3@gmail.com>
2025-01-31 11:07:22 -0500

    Ajouter labo objet

---
+++ Labo_Objet_1/Labo_Objet_1/Labo_Objet_1.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+#include <cstdio>
+#include <stdio.h>
+using namespace std;
+
+void exercice1(void) {
+	int num1;
+	int num2;
+	int num3;
+	int num4;
+
+	cout << "Entrez vos quatres nombres" << endl;
+
+	scanf("%d,%d,%d,%d", &num1, &num2, &num3, &num4);
+
+	int sum = num1 + num2 + num3 + num4;
+	int moyenne = sum / 4;
+	int produit = num1 * num2 * num3 * num4;
+	
+	for (int i = 1; i <= 4; i++) {
+
+	}
+  
+
+
+}
+
+
